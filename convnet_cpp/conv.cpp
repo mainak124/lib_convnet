8c452397378eb5e114e2dc9af85baf54f8885f70
Mainak Biswas <mainak124@gmail.com>
2016-05-08 17:07:50 -0700

    Initial Commit With Tensorflow Implementation

---
+++ convnet_cpp/conv.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+
+class _conv {
+    float params[][][][];
+    int stride, n_filters, zero_pad;
+    public:
+        _conv(int, int, );
+        conv2d(x);
+}; 
+
+_conv::_conv (float W[][][][], ) {
+    params = W;
+    stride = stride;
+    zero_pad = zero_pad;
+    n_filters = n_filters;
+}
+
+_conv::conv2d(x, n_in, n_row, n_col) {
+    
+    float out[][][];
+    
+    //for (filter_idx=0; filter_idx<n_filter; filter_idx++) 
+    for (in_ch_idx=0; in_ch_idx<n_in; in_ch_idx++){
+        f    
+    }
+    
+       
+}        
