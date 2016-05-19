4bc3546292e9f29053509ad2ba7e7260168b873b
Debanjan Chatterjee <dac261188@gmail.com>
2016-05-18 22:52:30 -0700

    updated template for conv; Added template for maxPoolNxN; datatype as typedef; updated testbench

--- convnet_cpp/conv.h
+++ convnet_cpp/conv.h
@@ -1,12 +1,89 @@
+
+typedef float DTYPE_T;
+
+
 template 
 <
-    int n_row_in,
-    int n_col_in,
-    int n_in,
-    int n_row_filter,
-    int n_col_filter,
-    int n_out_c,
-    int n_out_r,
-    int n_filter
+int n_row_in,
+int n_col_in,
+int n_in,
+int n_row_filter,
+int n_col_filter,
+int n_out_c,
+int n_out_r,
+int n_filter
 >    
-void conv2d(float x[n_row_in][n_col_in][n_in], float W[n_row_filter][n_col_filter][n_in][n_filter], float out_feature[n_out_r][n_out_c][n_filter], int stride, int zero_pad); 
+void conv2d(DTYPE_T x[n_row_in][n_col_in][n_in], DTYPE_T W[n_row_filter][n_col_filter][n_in][n_filter], DTYPE_T out_feature[n_out_r][n_out_c][n_filter], int stride, int zero_pad) {
+
+    DTYPE_T in_val, out;
+    int slide_in_r_idx, slide_in_c_idx, out_r_idx, out_c_idx;
+
+    // Treversing through spatial dimensions of the input
+    for (int in_r_idx=0; in_r_idx<n_row_in-n_row_filter+2*zero_pad+1; in_r_idx+=stride) {
+	    for (int in_c_idx=0; in_c_idx<n_col_in-n_col_filter+2*zero_pad+1; in_c_idx+=stride) {
+		    // Traversing through all output filters
+		    for (int filter_idx=0; filter_idx<n_filter; filter_idx++) { 
+			    out = 0;
+			    // Traversing through all input channels
+			    for (int in_ch_idx=0; in_ch_idx<n_in; in_ch_idx++) {
+				    // Traversing through spatial dimensions of the filters
+				    for(int r_idx=0; r_idx<n_row_filter; r_idx++) {
+					    for(int c_idx=0; c_idx<n_col_filter; c_idx++) {
+						    slide_in_r_idx = in_r_idx+r_idx - zero_pad;
+						    slide_in_c_idx = in_c_idx+c_idx - zero_pad;
+						    // zero padding
+						    //if ((slide_in_r_idx < 0) || (slide_in_r_idx >= n_row_in) || (slide_in_c_idx < 0) || (slide_in_c_idx >= n_col_in))
+						    //        in_val = 0;
+						    //else
+							    in_val = x[slide_in_r_idx][slide_in_c_idx][in_ch_idx];    
+						    out = out + (in_val * W[r_idx][c_idx][in_ch_idx][filter_idx]);
+							printf("%d,%d\t",slide_in_r_idx,slide_in_c_idx);
+					    }
+				    }
+			    }
+			    out_r_idx = in_r_idx/stride;
+			    out_c_idx = in_c_idx/stride;
+			    out_feature[out_r_idx][out_c_idx][filter_idx] = out;
+			printf("%d,%d\n",out_r_idx,out_c_idx);
+				
+		    }    
+	    }    
+    }    
+}
+
+template 
+<
+int n_row_in,
+int n_col_in,
+int n_in,
+int n_out_c,
+int n_out_r,
+int n_out,
+int n_pool_c,
+int n_pool_r
+>    
+void maxPoolNxN(DTYPE_T x[n_row_in][n_col_in][n_in], DTYPE_T out_feature[n_out_r][n_out_c][n_out]) {
+
+    DTYPE_T out;
+
+    // Treversing through spatial dimensions of the input
+    for (int in_r_idx=0; in_r_idx<n_row_in; in_r_idx+=n_pool_r) {
+	    for (int in_c_idx=0; in_c_idx<n_col_in; in_c_idx+=n_pool_c) {
+		    // Traversing through all input channels
+		    for (int in_ch_idx=0; in_ch_idx<n_in; in_ch_idx++) {
+			    // Traversing through spatial dimensions of the filters
+				out = 0;
+			    for(int r_idx=0; r_idx<n_pool_r; r_idx++) {
+				    for(int c_idx=0; c_idx<n_pool_c; c_idx++) {
+						if(x[in_r_idx+r_idx][in_c_idx+c_idx][in_ch_idx] > out)
+							out = x[in_r_idx+r_idx][in_c_idx+c_idx][in_ch_idx];
+				    }
+			    }
+			    int out_r_idx = in_r_idx/n_pool_r;
+			    int out_c_idx = in_c_idx/n_pool_c;
+			int out_ch_idx = in_ch_idx;
+			    out_feature[out_r_idx][out_c_idx][out_ch_idx] = out;
+		    }    
+	    }    
+    }    
+}
--- convnet_cpp/conv_test.cpp
+++ convnet_cpp/conv_test.cpp
@@ -1,15 +1,79 @@
+#include <stdio.h>
+#include <cstdlib>
 #include "conv.h"
-
+//#include "conv.cpp"
 int main() {
 
-    float x_in[10][10][1];
-    float W[5][5][1][2];
-    float conv_out[6][6][2];
+    DTYPE_T x_in[10][10][1];
+    DTYPE_T W[3][3][1][2];
+    DTYPE_T conv_out[8][8][2];
+    DTYPE_T pool_out[4][4][2];
     int zeropad = 0;
     int stride = 1;
 
+	int init_cnt = 0;
+
     // Load values
+for (int i = 0; i< 10; i++){
+for (int j = 0; j< 10; j++){
+for (int k = 0; k< 1; k++){
+	x_in[i][j][k] = init_cnt++;//static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}}}
+init_cnt = 0;
+for (int i = 0; i< 3; i++){
+for (int j = 0; j< 3; j++){
+for (int k = 0; k< 1; k++){
+for (int l = 0; l< 2; l++){
+	W[i][j][k][l] = init_cnt++;//static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}}}}
+//for (int i = 0; i< 8; i++){
+//for (int j = 0; j< 8; j++){
+//for (int k = 0; k< 2; k++){
+//	conv_out = rand();
+//}}}
+    conv2d<10,10,1,3,3,8,8,2>(x_in, W, conv_out, stride, zeropad);
+    maxPoolNxN<8,8,2,4,4,2,2,2>(conv_out,pool_out);
 
-    conv2d(x_in, W, conv_out, stride, zeropad);
 
+for (int i = 0; i< 10; i++){
+for (int j = 0; j< 10; j++){
+for (int k = 0; k< 1; k++){
+	printf("%f, ",x_in[i][j][k]); //x_in = rand();
+}
+}
+printf("\n");
+}
+printf("\n");
+printf("\n");
+for (int i = 0; i< 3; i++){
+for (int j = 0; j< 3; j++){
+for (int k = 0; k< 1; k++){
+for (int l = 0; l< 2; l++){
+	printf("%f, ",W[i][j][k][l]); //x_in = rand();
+}}
+//printf("\n");
+}
+printf("\n");
+}
+
+printf("\n");
+printf("\n");
+
+for (int i = 0; i< 8; i++){
+for (int j = 0; j< 8; j++){
+for (int k = 0; k< 2; k++){
+	printf("%f, ",conv_out[i][j][k]); //x_in = rand();
+}
+}
+printf("\n");
+}
+
+for (int i = 0; i< 4; i++){
+for (int j = 0; j< 4; j++){
+for (int k = 0; k< 2; k++){
+	printf("%f, ",pool_out[i][j][k]); //x_in = rand();
+}
+}
+printf("\n");
+}
 }
